311936c04e3598f5f22b446946ccbe35b8959c2a
Gayane Grigoryan <44817376+Gayane05@users.noreply.github.com>
2024-12-16 18:57:14 +0400

    Add files via upload
    Finished encoding part: Initial version of compression

--- Huffman EncoderDecoder.cpp
+++ Huffman EncoderDecoder.cpp
@@ -1,4 +1,4 @@
-// Huffman EncoderDecoder.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Huffman EncoderDecoder.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <unordered_map>
 #include <queue>
+#include <bitset>
 
 #include "Element.h"
 //namespace fs = std::filesystem;
@@ -14,6 +15,17 @@
 //{
 //   return left.weight < right.weight;
 //}
+
+
+int binaryToDecimal(const std::string& in)
+{
+   int result = 0;
+   for (int i = 0; i < in.size(); i++)
+      result = result * 2 + in[i] - '0';
+   return result;
+}
+
+
 
 void readContentFromFile(std::string& fileContent, std::string& fileName, std::string& /*filePathName*/)
 {
@@ -33,7 +45,9 @@ void readContentFromFile(std::string& fileContent, std::string& fileName, std::s
    file.close();
 }
 
-void createFrequencyHeap(std::priority_queue<Element, std::vector<Element>, std::greater<Element>>& queue, const std::string& contentString)
+// Store different characters frequencies.
+void createFrequencyHeap(std::priority_queue<Element, std::vector<Element>, std::greater<Element>>& queue,
+   const std::string& contentString)
 {
    std::unordered_map<char, long long> freqTable;
    for (const auto symbol : contentString)
@@ -47,6 +61,7 @@ void createFrequencyHeap(std::priority_queue<Element, std::vector<Element>, std:
    }
 }
 
+// Construct Priority Queue and Huffman Tree
 Element* creatingBinaryTree(std::priority_queue<Element, std::vector<Element>, std::greater<Element>>& elements)
 {
    // Taking the smallest 2 elements to join them in one Element.
@@ -78,7 +93,8 @@ Element* creatingBinaryTree(std::priority_queue<Element, std::vector<Element>, s
 
 std::unordered_map<char, std::string> prefixCodeTable;
 
-void createPrefixCodeTable(Element* root, char symbol, std::string& path)
+// Create Huffman Encode Table
+void createPrefixCodeTable(Element* root, std::string& path)
 {
    if (!root)
    {
@@ -90,39 +106,57 @@ void createPrefixCodeTable(Element* root, char symbol, std::string& path)
    if (!root->leftChild && !root->rightChild)
    {
       std::cout << root->symbol << " " << path << "\n";
-      prefixCodeTable[symbol] = path;
+      prefixCodeTable[root->symbol] = path;
    }
 
-   createPrefixCodeTable(root->leftChild, symbol, path);
-   createPrefixCodeTable(root->rightChild, symbol, path);
+   createPrefixCodeTable(root->leftChild, path);
+   createPrefixCodeTable(root->rightChild, path);
    if (path.size())
    {
       path.pop_back();
    }
 }
 
-void printTree(Element* root)
-{
-   if (root)
-   {
-      printTree(root->leftChild);
-      //    path += root->path;
-      std::cout << root->symbol << "  " << root->path << "\n";
-
-      printTree(root->rightChild);
-   }
-}
-
-void outputInFile()
+void saveInFile(std::string& initialText)
 {
    std::cout << "Insert output file name ";
    std::string fileName;
    std::cin >> fileName;
 
+   std::ofstream outputFile(fileName, std::ofstream::out);
+
+   for (const auto& it : prefixCodeTable)
+   {
+      outputFile << it.first << "-" << it.second << "\\";
+   }
+
+   std::string encodedText;
+
+
+   for (size_t i = 0; i < initialText.size(); ++i)
+   {
+      encodedText += prefixCodeTable[initialText[i]];  // Taking prefix code from table for concrete character.
+
+      //std::bitset<3> b(path);
+      //  encodedText += b.to_string();
+   }
+
+   std::cout << "Encoded text " << encodedText  << " Total Bytes : " << encodedText.size() << '\n';
+
+   std::string encodedPackedText;
+
+   for (size_t i = 0; i < encodedText.size(); i +=8)
+   {
+      int number = binaryToDecimal(encodedText.substr(i, 8));  // Number as result of packed bits.
+      encodedPackedText += std::to_string(number);
+   }
+
+   std::cout << "Encoded packed text " << encodedPackedText << " Total Bytes : " << encodedPackedText.size() << '\n';
+
+   outputFile.close();
 }
 
-
-int main()
+void encoding()
 {
    std::cout << "Insert file path ";
    std::string filePath;
@@ -132,18 +166,49 @@ int main()
    std::string fileName;
    std::cin >> fileName;
 
-   std::string fileContent;
-   readContentFromFile(fileContent, fileName, filePath);
+   std::string intialText;
+   readContentFromFile(intialText, fileName, filePath);
+
+   std::cout << "Initial text " << intialText << " Total Bytes : " << intialText.size() << '\n';
 
    std::priority_queue<Element, std::vector<Element>, std::greater<Element>> elementsHeap;
-   createFrequencyHeap(elementsHeap, fileContent);
+   createFrequencyHeap(elementsHeap, intialText);
 
-   Element * rootNode = creatingBinaryTree(elementsHeap);
+   Element* rootNode = creatingBinaryTree(elementsHeap);
 
    std::string str;
-   createPrefixCodeTable(rootNode, 'x', str);
+   createPrefixCodeTable(rootNode, str);
 
-   printTree(rootNode);
+
+   saveInFile(intialText);
+}
+
+void decoding()
+{
+   std::cout << "Insert file path ";
+   std::string filePath;
+   //std::cin >> filePath;
+
+   std::cout << "Insert file name ";
+   std::string fileName;
+   std::cin >> fileName;
+
+   //std::string encodedText;
+
+   //readContentFromFile(encodedText, fileName, filePath);
+
+
+   //const auto headerEndsPos = std::find(encodedText.rbegin(), encodedText.rend(), '\\');
+
+}
+
+
+int main()
+{
+   encoding();
+
+ //  decoding();
+
 
    return 0;
 }
